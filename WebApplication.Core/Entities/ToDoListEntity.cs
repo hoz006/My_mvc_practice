93769cb2b8039acd69add98c242ffcbef873145d
StoneShiLei <569812422@qq.com>
2022-03-29 10:20:30 +0800

    4

---
+++ WebApplication.Core/Entities/ToDoListEntity.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication.Core.Enum;
+using Newtonsoft.Json;
+
+namespace WebApplication.Core.Entities
+{
+    public class ToDoListEntity
+    {
+        /// <summary>
+        /// 主键id
+        /// </summary>
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 内容
+        /// </summary>
+        [JsonProperty("content")]
+        public string Content { get; set; }
+
+        /// <summary>
+        /// 是否完成
+        /// </summary>
+        [JsonProperty("isDone")]
+        public bool IsDone { get; set; }
+
+        /// <summary>
+        /// 类型
+        /// </summary>
+        [JsonProperty("type")]
+        public ToDoListType Type { get; set; }
+
+        /// <summary>
+        /// 创建日期
+        /// </summary>
+        [JsonProperty("createdDate")]
+        public DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        [JsonProperty("creatorName")]
+        public string CreatorName { get; set; }
+    }
+}
---
+++ WebApplication.Core/Enum/ToDoListType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication.Core.Enum
+{
+    public enum ToDoListType
+    {
+        [Description("A级")]
+        LevelA = 0,
+
+        [Description("B级")]
+        LevelB = 1,
+
+        [Description("C级")]
+        LevelC = 2,
+    }
+}
